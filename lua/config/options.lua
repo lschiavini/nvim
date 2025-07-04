cf1d8abc28fc97df558f98dad7c7738e134080dc
luskira <lucas.s.schiavini@gmail.com>
2025-07-04 16:37:39 -0300

    transparency and tab spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -22,6 +22,11 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+-- tabspace
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,17 +1,20 @@
 return {
 	{
-		-- Theme inspired by Atom
 		'navarasu/onedark.nvim',
 		priority = 1000,
 		lazy = false,
 		config = function()
 			require('onedark').setup {
-				-- Set a style preset. 'dark' is default.
-				style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+				style = 'dark',
+				transparent = true, -- Enable transparent background
+				term_colors = true, -- Use terminal colors for consistency
 			}
 			require('onedark').load()
 			vim.cmd.colorscheme('onedark')
 			vim.opt.background = 'dark'
+			-- Ensure Normal and NormalFloat backgrounds are transparent
+			vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+			vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 		end,
 	},
 }
