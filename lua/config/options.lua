558d33bdb6ccf72b0f35d3f74b0d9c03b28103cd
luskira <lucas.s.schiavini@gmail.com>
2025-08-13 17:14:02 -0300

    adding tabs and oil

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,6 +39,7 @@ vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
+vim.o.showtabline = 2
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
